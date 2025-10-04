3d1967ad64b12d994a6212dff6eb9d2c136e74b3
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-10-04 11:25:52 +0530

    Day 1: Added Footer component

--- Day 1/Topic-1/my-app/src/App.jsx
+++ Day 1/Topic-1/my-app/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Footer from "./Footer";
 function App(){
   return(
     <div>
       <h1>Hello React</h1>
       <Header/>
+      <Footer/>
     </div>
 
   );
---
+++ Day 1/Topic-1/my-app/src/Footer.jsx
@@ -0,0 +1,6 @@
+function Footer(){
+  return(
+    <h2>© 2025 My First React App</h2>
+  );
+}
+export default Footer;
